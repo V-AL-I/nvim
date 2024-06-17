21f6f492d4ac5c268491d1e57d2c16d66ab53208
Felix Muhlke <felix.muhlke@epita.fr>
2024-06-17 10:46:15 +0200

    added persistent undo, backup and swap

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,36 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " "
+
+USER = os.getenv("USER")
+SWAPDIR = "/home/" .. USER .. "/.nvim/swap//"
+BACKUPDIR = "/home/" .. USER .. "/.nvim/backup//"
+UNDODIR = "/home/" .. USER .. "/.nvim/undo//"
+
+if vim.fn.isdirectory(SWAPDIR) == 0 then
+	vim.fn.mkdir(SWAPDIR, "p", "0o700")
+end
+
+if vim.fn.isdirectory(BACKUPDIR) == 0 then
+	vim.fn.mkdir(BACKUPDIR, "p", "0o700")
+end
+
+if vim.fn.isdirectory(UNDODIR) == 0 then
+	vim.fn.mkdir(UNDODIR, "p", "0o700")
+end
+
+-- Enable swap, backup, and persistant undo
+vim.opt.directory = SWAPDIR
+vim.opt.backupdir = BACKUPDIR
+vim.opt.undodir = UNDODIR
+vim.opt.swapfile = true
+vim.opt.backup = true
+vim.opt.undofile = true
+
+-- Append backup files with timestamp
+vim.api.nvim_create_autocmd("BufWritePre", {
+	callback = function()
+		local extension = "~" .. vim.fn.strftime("%Y-%m-%d-%H%M%S")
+		vim.o.backupext = extension
+	end,
+})

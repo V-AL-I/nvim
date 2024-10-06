380ff8d3ea48a66c7321b26e33c69913621d952e
VALI <thomas.solatges@gmail.com>
2024-10-06 13:19:08 +0200

    unbind arrows

--- init.lua
+++ init.lua
@@ -43,3 +43,20 @@ vim.api.nvim_set_keymap('n', '<leader>f', ':Telescope fd<CR>', { noremap = true,
 vim.api.nvim_set_keymap('n', '<leader>s', ':vsplit <CR>', { noremap = true, silent = true })
 
 
+-- Disable arrow keys in normal mode
+vim.api.nvim_set_keymap('n', '<Up>',    '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Down>',  '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Left>',  '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Right>', '<NOP>', { noremap = true, silent = true })
+
+-- Disable arrow keys in insert mode
+vim.api.nvim_set_keymap('i', '<Up>',    '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Down>',  '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Left>',  '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Right>', '<NOP>', { noremap = true, silent = true })
+
+-- Disable arrow keys in visual mode
+vim.api.nvim_set_keymap('v', '<Up>',    '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Down>',  '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Left>',  '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Right>', '<NOP>', { noremap = true, silent = true })
